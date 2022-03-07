80e21dcbb8cb014c516c09c4a08969e93acbf888
Aidan Brubeck <50123545+Ayydeeen@users.noreply.github.com>
2022-03-07 13:13:58 -0500

    Update query.ps1

--- query.ps1
+++ query.ps1
@@ -1,1 +1,6 @@
+Connect-AzureAD:
 Get-AzureADUser -All $true -Filter "userType eq 'Member' and accountEnabled eq true" | select displayname, userprincipalname | sort displayname | export-csv .\365Users.csv
+
+AD/LDAP Builtin:
+Get-ADUser -LDAPFilter '(!userAccountControl:1.2.840.113556.1.4.803:=2)' | export-csv C:\Temp\Enabled.csv
+Get-ADUser -LDAPFilter '(!userAccountControl:1.2.840.113556.1.4.803:=1)' | export-csv C:\Temp\Disabled.csv
